a82d1018ddae323485861159b40ca2e54c15c788
Alexandr Mishuk <mishasashuk@gmail.com>
2026-02-09 21:05:39 +0300

    feat: add solution of task2

--- task2.ts
+++ task2.ts
@@ -1,4 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Написать функцию, которая складывает все четные числа и возвращает результат
 
 const array: number[] = [4, 33, 45, 20, 57, 0, 2];
+
+const sumEven = (arr: number[]) => {
+  return arr.reduce((acc, value) => {
+    if (!(value % 2)) {
+      return acc + value;
+    }
+    return acc;
+  }, 0);
+};
+console.log(sumEven(array));
