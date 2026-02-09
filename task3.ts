f72fe4e1fe9752d9bfced0d60168d048e6e30703
Alexandr Mishuk <mishasashuk@gmail.com>
2026-02-09 21:31:53 +0300

    feat: add solution of task3

--- task3.ts
+++ task3.ts
@@ -1,5 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Напишите функцию, которая проверяет не содержит ли слово повторяющихся букв
 
 const str1 = "Android";
 const str2 = "Iphone";
+
+const noDuplicates = (word: string) => new Set(word.toLowerCase()).size === word.length;
+console.log(noDuplicates(str1));
+console.log(noDuplicates(str2));
