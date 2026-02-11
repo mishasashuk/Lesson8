ddf7417ba6f021ebe6b4d37d4c1aa85fe7676498
Alexandr Mishuk <mishasashuk@gmail.com>
2026-02-11 15:48:15 +0300

    feat: add solution of task4

--- task4.ts
+++ task4.ts
@@ -1,6 +1,9 @@
-/* eslint-disable prefer-const */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Написать функцию, которая поменяет переменные местами, не создавая дополнительную переменную
 
 let num1 = 5;
 let num2 = 7;
+
+[num1, num2] = [num2, num1];
+
+console.log(num1);
+console.log(num2);
